cd8f536ed16b87d94097bd00c7052424e17ff81e
Khadija Ahmada <kahmada@e3r2p2.1337.ma>
2025-07-09 13:39:41 +0100

    add routing

--- next/app/components/Header.tsx
+++ next/app/components/Header.tsx
@@ -1,17 +1,21 @@
-
+import Link from 'next/link';
 import './Header.css';
 export default function Header() {
     return (
       <header>
-        <nav>
-          <ul>
-            <li>home</li>
-            <li>about</li>
-            <li>services</li>
-            <li>contact</li>
-            <li>else</li>
-          </ul>
-        </nav>
+      <nav>
+        <ul>
+        <li>home</li>
+        <li>
+          <Link href="/about">
+          About
+          </Link>
+        </li>
+        <li>services</li>
+        <li>contact</li>
+        <li>else</li>
+        </ul>
+      </nav>
       </header>
     );
   }
